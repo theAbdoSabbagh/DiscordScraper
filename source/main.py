ac4a2ffcc6d395a840a3e3bb765ab5c83e0c0d43
Sxvxgee <aboud1xy@gmail.com>
2022-09-22 17:59:33 +0400

    code works well

--- source/main.py
+++ source/main.py
@@ -1,7 +1,8 @@
 from utils.useful import show_header, check_config_file, Logger, get_account_settings
 from utils.selfbot import bot
 
-show_header(); check_config_file()
+show_header()
+check_config_file()
 log = Logger(bot)
 config = get_account_settings()
 
